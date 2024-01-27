4fb6b6c4e4e01a78a14f3532a237992b5d75fe57
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-28 00:10:09 +0100

    worked on grid and background images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,64 +9,16 @@ export function HomePage() {
         </div>
 
         <div className="item-1">
-          <p>scale one</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            dolor in velit eius, tenetur quibusdam debitis mollitia sed ea
-            sequi, quisquam totam voluptatibus suscipit explicabo. Porro quae
-            eum ratione laborum!
-          </p>
+          <h3>Guitar session in Talbohina, Odessa</h3>
         </div>
         <div className="item-2">
-          <p>scale 2</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-            quis, sapiente vel ullam nisi soluta nesciunt ipsa, omnis nulla,
-            quas dolorem repudiandae in odio exercitationem hic veniam illo et
-            quo sint natus blanditiis mollitia laborum. Voluptatem,
-            necessitatibus pariatur enim assumenda ipsam laudantium suscipit
-            molestias totam eius, recusandae rerum? Corporis odio ab commodi
-            eaque laudantium velit, et cumque. Eaque, ratione culpa, iusto
-            voluptates sed aspernatur, quam quis doloribus explicabo odit
-            voluptatum nam illo inventore? Repudiandae quod harum fugit ullam
-            rem! Sapiente, quaerat voluptas? Nam eum id culpa ut quidem.
-            Consectetur totam ad impedit voluptas ipsam suscipit quae facilis ea
-            tempore quam!
-          </p>
+          <h3>Mole national park, Ghana</h3>
         </div>
         <div className="item-3">
-          <p>Scale 3</p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-            consequuntur quo possimus, officiis qui culpa odio? Accusantium
-            distinctio veritatis autem sapiente excepturi maiores, voluptatem
-            aperiam recusandae illum asperiores reiciendis culpa explicabo?
-            Debitis, repellendus esse quam officia accusantium animi laboriosam
-            vitae quod ipsum qui ad iste quo dolore eaque dolores expedita, est
-            laudantium excepturi cum placeat facilis aut sequi? Exercitationem
-            eum enim soluta assumenda nihil nam fugiat maxime facilis deserunt
-            quia aut, voluptatum harum tempore quod ullam dolores velit
-            doloremque. Qui quasi est magnam quas ullam vitae officia facere
-            quia, culpa a expedita voluptas dolorum nihil itaque repellendus
-            corporis alias asperiores!
-          </p>
+          <h3>Chilling next to the Black Sea</h3>
         </div>
         <div className="item-4">
-          <p>Scale 3</p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-            consequuntur quo possimus, officiis qui culpa odio? Accusantium
-            distinctio veritatis autem sapiente excepturi maiores, voluptatem
-            aperiam recusandae illum asperiores reiciendis culpa explicabo?
-            Debitis, repellendus esse quam officia accusantium animi laboriosam
-            vitae quod ipsum qui ad iste quo dolore eaque dolores expedita, est
-            laudantium excepturi cum placeat facilis aut sequi? Exercitationem
-            eum enim soluta assumenda nihil nam fugiat maxime facilis deserunt
-            quia aut, voluptatum harum tempore quod ullam dolores velit
-            doloremque. Qui quasi est magnam quas ullam vitae officia facere
-            quia, culpa a expedita voluptas dolorum nihil itaque repellendus
-            corporis alias asperiores!
-          </p>
+          <h3>Odessa morning sunrise</h3>
         </div>
       </div>
     </>
