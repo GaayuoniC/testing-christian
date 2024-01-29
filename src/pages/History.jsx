424997d99840627f5cc7d7e074560df0938a77a2
Christian Gaayuoni <104912138+ChrisGaay@users.noreply.github.com>
2024-01-29 10:01:18 +0100

    Work in progress on grid and flex display for flexibility

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -2,60 +2,42 @@ import "./History.css";
 export function History() {
   return (
     <div className="history-container">
-      <div className="history-item-header">
+      <div className="history-item">
         <h1> History </h1>
       </div>
-      <div className="history-item-1">
-        one
+      <div className="history-item">
+        <p> one </p>
         <article>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde velit
-            a corporis quisquam impedit aperiam at. Neque recusandae ratione
-            iure odit eius odio ut minus dolorum ad reiciendis! Beatae,
-            praesentium.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            nemo ex, fugiat iusto iste veritatis. Placeat voluptas reiciendis ab
+            et quis provident ducimus totam amet consectetur minima quae,
+            cumque, accusantium iste. Ipsum, corrupti voluptate? Praesentium
+            inventore pariatur accusantium vitae voluptatum consequuntur vel
+            consectetur quasi aliquid odio, exercitationem voluptas harum non.
           </p>
         </article>
       </div>
-      <div className="history-item-2">
-        <div className="history-item-2-grid-item-1">
+      <div className="history-item">
+        <div>
           <article>
             <p> two</p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              nemo ex, fugiat iusto iste veritatis. Placeat voluptas reiciendis
-              ab et quis provident ducimus totam amet consectetur minima quae,
-            </p>
-          </article>
-        </div>
-        <div className="history-item-2-grid-item-2">
-          <article>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              nemo ex, fugiat iusto iste veritatis. Placeat voluptas reiciendis
-              ab et quis provident ducimus totam amet consectetur minima quae,
-            </p>
-          </article>
-        </div>
-        <div className="history-item-2-grid-item-3">
-          <article>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              nemo ex, fugiat iusto iste veritatis. Placeat voluptas reiciendis
-              ab et quis provident ducimus totam amet consectetur minima quae,
-            </p>
-          </article>
-        </div>
-        <div className="history-item-2-grid-item-4">
-          <article>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              nemo ex, fugiat iusto iste veritatis. Placeat voluptas reiciendis
-              ab et quis provident ducimus totam amet consectetur minima quae,
+              1 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Ducimus nemo ex, fugiat iusto iste veritatis. Placeat voluptas
+              reiciendis ab et quis provident ducimus totam amet consectetur
+              minima quae, Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Unde velit a corporis quisquam impedit aperiam at. Neque
+              recusandae ratione iure odit eius odio ut minus dolorum ad
+              reiciendis! Beatae, praesentium. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Ducimus nemo ex, fugiat iusto iste
+              veritatis. Placeat voluptas reiciendis ab et quis provident
+              ducimus totam amet consectetur minima quae,
             </p>
           </article>
         </div>
       </div>
-      <div className="history-item-3">
+      <div className="history-item">
         <p>three</p>
         <article>
           <p>
@@ -78,7 +60,7 @@ export function History() {
           </p>
         </article>
       </div>
-      <div className="history-item-4">
+      <div className="history-item">
         four
         <article>
           <p>
@@ -91,7 +73,7 @@ export function History() {
           </p>
         </article>
       </div>
-      <div className="history-item-5">
+      <div className="history-item">
         five
         <article>
           <p>
@@ -104,17 +86,16 @@ export function History() {
           </p>
         </article>
       </div>
-      <div className="history-item-6">
+      <div className="history-item">
         six
         <article>
-          {" "}
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
             nemo ex, fugiat iusto iste veritatis. Placeat voluptas reiciendis ab
             et quis provident ducimus totam amet consectetur minima quae,
             cumque, accusantium iste. Ipsum, corrupti voluptate? Praesentium
             inventore pariatur accusantium vitae voluptatum consequuntur vel
-            consectetur quasi aliquid odio, exercitationem voluptas harum non.{" "}
+            consectetur quasi aliquid odio, exercitationem voluptas harum non.
           </p>
         </article>
       </div>
